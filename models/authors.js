f34c70b5831aaaf98ba5cf8848a0f60ea0bc098e
cmdenault <78628407+cmdenault@users.noreply.github.com>
2024-05-08 00:22:09 -0700

    Create authors.js

---
+++ models/authors.js
@@ -0,0 +1,22 @@
+const authors = [
+    { // new author obj
+        id: 1,
+        name: 'Drew Soo',
+        birthDate: '02/17/1960',
+        bio: 'A novelist',
+        genre: 'Mystery',
+
+    },
+
+    { // new author obj
+        id: 2,
+        name: 'Reed Granger',
+        birthDate: '01/28/2005',
+        bio: 'A strikingly poignant writer',
+        genre: 'Memoir'
+
+    }
+
+]
+
+module.exports = authors
